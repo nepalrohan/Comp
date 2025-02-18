1b9aa0a1e5eaa5ec96adaf76ea221408bdc10983
nepalrohan <rohannepal098@gmail.com>
2025-02-18 22:30:32 +0545

    simple setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+   <div>
+   <h1 className="text-center text-6xl text-cyan-700 font-extrabold">hello from lenovo slim3 ideapd! hprocessor</h1>
+   </div>
+  );
+}
