47a15d47113b3781b25827efb7cfb790a8dc6fe1
nepalrohan <rohannepal098@gmail.com>
2025-02-19 21:57:30 +0545

    add views in typescript-learning purpose

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,14 +1,16 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("dotenv/config");
-const app = (0, express_1.default)();
+import express from 'express';
+import "dotenv/config";
+import path from 'path';
+import { fileURLToPath } from 'url';
+const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render("welcome");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
